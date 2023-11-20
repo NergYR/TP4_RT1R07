e054dc429d3c175470ae9e07938aff42c702f13f
energetiq <energetiq@outlook.com>
2023-11-20 18:32:03 +0100

    Update MAX constant and add functions for input and display of an array

--- src/main.c
+++ src/main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
-#define MAX 10
+#define MAX 30
 
-float tabentier[MAX];
+int tabentier[MAX];
 
-
-int main(){
+void afficheTab(int n, int tab[]){
     int i;
-    for (i=0; i < MAX; i++){
-        printf("Entrez un nombre : ");
-        scanf("%f", &i);
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", tab[i]);
     }
+}
+void saisieTab(int n, int tab[]){
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("Entrez un nombre : ");
+        scanf("%d", &tab[i]);
+    }
+}
+
+int main()
+{
+    printf("Quel est la taille du tableau : ");
+    int n;
+    scanf("%d", &n);
+    int tab[n];
+    saisieTab(n, tab);
+    afficheTab(n, tab);
+
 }
